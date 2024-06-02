eeb5568f9c871137ddb01b147e37a0cc95e7270b
Kody Robinson <youcanhavesandwich@gmail.com>
2024-06-02 15:25:23 +1000

    Finish project

--- javascript.js
+++ javascript.js
@@ -4,6 +4,9 @@ let humanScore = 0;
 let computerScore = 0;
 let userInput = "";
 let computerChoice = "";
+const rockAudio = new Audio('images/throwing-a-rock-on-ice-192882.mp3')
+const paperAudio = new Audio('images/paper-rustling-97867.mp3')
+const scissorsAudio = new Audio('images/scissors-quick-cut-100455.mp3')
 
 const buttonRock = document.getElementById("buttonRock");
 const buttonPaper = document.getElementById("buttonPaper");
@@ -17,18 +20,20 @@ const startButton = document.getElementById('start')
 
 
 buttonRock.addEventListener('click', function(){
-    document.getElementById('text').innerHTML = "You chose Rock!";
+    rockAudio.play();
+    document.getElementById('humanScore').innerHTML = "You chose Rock!";
     computerChoice = getComputerChoice();
-    document.getElementById('computerText').innerHTML =  "Computer chose: " + computerChoice;
+    document.getElementById('computerScore').innerHTML =  "Computer chose: " + computerChoice;
     userInput = "Rock";
     playRound()
     gameScore()
 });
 
 buttonPaper.addEventListener('click', function(){
-    document.getElementById('text').innerHTML = "You chose Paper!";
+    paperAudio.play();
+    document.getElementById('humanScore').innerHTML = "You chose Paper!";
     computerChoice = getComputerChoice();
-    document.getElementById('computerText').innerHTML =  "Computer chose: " + computerChoice;
+    document.getElementById('computerScore').innerHTML =  "Computer chose: " + computerChoice;
     userInput = "Paper";
     playRound()
     gameScore()
@@ -36,9 +41,10 @@ buttonPaper.addEventListener('click', function(){
 });
 
 buttonScissors.addEventListener('click', function(){
-    document.getElementById('text').innerHTML = "You chose Scissors!";
+    scissorsAudio.play();
+    document.getElementById('humanScore').innerHTML = "You chose Scissors!";
     computerChoice = getComputerChoice();
-    document.getElementById('computerText').innerHTML =  "Computer chose: " + computerChoice;
+    document.getElementById('computerScore').innerHTML =  "Computer chose: " + computerChoice;
     userInput = "Scissors";
     gameScore()
     playRound()
@@ -113,19 +119,22 @@ function playRound(){
 
 
 let gameScore = function(){
-    if (humanScore === 4){
+    if (humanScore === 5){
         console.log("You win, Congratulations")
         document.getElementById('buttons').innerHTML = 
         "<div id='textBox'><span style='font-size:60px'>You win, Congratulations</span></div>";
-    } else if (computerScore === 4){
+    } else if (computerScore === 5){
         console.log("computer wins")
         document.getElementById('buttons').innerHTML = 
         "<div id='textBox'><span style='font-size:60px'>Computer wins, try again!</span></div>";
     } 
-    else if (humanScore || computerScore < 4){
+    else if (humanScore || computerScore < 5){
     console.log("play again")
     } 
 }
+
+
+// Restart page
 
 const restartBtn = document.getElementById("restart");
 
